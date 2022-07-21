aad7cfe5aabcdf9c029d83e485c4287a0a4e9794
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-07-21 12:42:09 -0300

    chore: add eslint with airbnb style guide

--- src/app.js
+++ src/app.js
@@ -10,5 +10,5 @@ app.use(express.json());
 
 const { PORT } = process.env;
 app.listen(PORT, () => {
-  console.log('App running on port: ' + PORT);
+  console.log(`App running on port: ${PORT}`);
 });
